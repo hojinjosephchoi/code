8e23a814a2212ac1d4bb56a447a7352fdb6e71e3
Hojin Choi <hojinchoi@Hojinui-MacBook-Pro.local>
2017-07-05 01:04:01 +0900

    chapter6 주석문 추가

--- chapter6/update.sh
+++ chapter6/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
+# 실행중인 EC2 인스턴스의 모든 공개이름 (public name) 얻기
 PUBLICNAMES=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].PublicDnsName" --output text)
 
 for PUBLICNAME in $PUBLICNAMES; do
